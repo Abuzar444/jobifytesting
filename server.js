36ee98201800a207385afdf2e21327b673226890
= <=>
2023-04-21 13:08:40 +0500

    sec-commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// import cors from 'cors';
 import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
@@ -6,20 +5,16 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
-// production setup
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// security packages
 import helmet from 'helmet';
 import xss from 'xss-clean';
 import mongoSanitize from 'express-mongo-sanitize';
-
-// cookie-parser
 import cookieParser from 'cookie-parser';
-
-// db and authenticate
+// hello
+// db and authenticateUser
 import connectDB from './db/connect.js';
 
 // routers
@@ -40,33 +35,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // only when ready to deploy
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
-// app.use(cors());
 app.use(express.json());
-// cookie-parser
-app.use(cookieParser());
-// security packages
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
+app.use(cookieParser());
 
-// routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 // only when ready to deploy
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-
-app.get('/', (req, res) => {
-  res.json('Welcome!');
-});
-app.get('/api/v1', (req, res) => {
-  res.json({ msg: 'API' });
-});
-
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -76,7 +57,9 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => console.log(`App is listening on port ${port}...`));
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
   } catch (error) {
     console.log(error);
   }
